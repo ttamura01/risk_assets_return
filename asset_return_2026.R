3170671a1d438c3f0e7651c1c3fdd2978c169827
ttamura01git config --global user.email ttamura010164@me.com~git remote add origin https://github.com/ttamura01/git-fed_rate_cut.gitgit push origin mainls <ttamura010164@me.com>
2026-02-24 07:53:32 -0500

    daily updates and some clean-up

--- asset_return_2026.R
+++ asset_return_2026.R
@@ -18,7 +18,6 @@ return <- read_csv("asset_return_2026.csv")
 
 # return %>% slice_max(date)
 
-
 head(return)
 tail(return)
 
@@ -26,17 +25,14 @@ tail(return)
 
 return$date <- as.Date(return$date, format = "%Y-%m-%d")
 
-daily <- "2026-02-19	0.002	-0.024	-0.033	-0.085	-0.112	-0.176	-0.023	-0.041	0.008	-0.044	-0.010	0.043	0.160	0.161	-0.226
-2026-02-20	0.009	-0.015	0.004	-0.084	-0.090	-0.179	-0.007	-0.027	0.018	-0.035	-0.006	0.049	0.182	0.161	-0.236"
+daily <- "2026-02-23	-0.001	-0.026	-0.007	-0.111	-0.111	-0.205	-0.035	-0.021	0.027	-0.076	-0.051	0.015	0.214	0.159	-0.269"
 daily <- gsub("\\s+", ",", daily)
 daily
 
 updates <- tribble(~date, ~`S&P500`, ~"NASDAQ", ~"Google",  ~"Tesla", ~"Amazon", ~"Microsoft",  ~"Meta",  ~"Apple", ~"NVIDIA", ~`JP Morgan`, ~"Citigroup", ~`Goldman Sachs`,  ~"Gold", ~"WTI", ~"Bitcoin",
-                   "2026-02-19",0.002,-0.024,-0.033,-0.085,-0.112,-0.176,-0.023,-0.041,0.008,-0.044,-0.010,0.043,0.160,0.161,-0.226,
-                   "2026-02-20",0.009,-0.015,0.004,-0.084,-0.090,-0.179,-0.007,-0.027,0.018,-0.035,-0.006,0.049,0.182,0.161,-0.236)
+                   "2026-02-23",-0.001,-0.026,-0.007,-0.111,-0.111,-0.205,-0.035,-0.021,0.027,-0.076,-0.051,0.015,0.214,0.159,-0.269)
 
 updates$date <- as.Date(updates$date, format = "%Y-%m-%d")
-# 
 
 return <- rbind(return, updates)
 
@@ -58,7 +54,6 @@ return$date <- as.Date(return$date, "%Y-%m-%d")
 
 return_long <- return %>% 
   pivot_longer(cols = - date, names_to = "asset", values_to = "price") 
-
 
 latest_date <- max(return_long$date)
 
